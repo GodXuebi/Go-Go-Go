95b24106f6fa94cd346c528018402a9c525e8ce0
God-Xuebi <2685043624@qq.com>
2021-04-19 21:18:18 +0800

    rpc server and client based on tcp

--- Go_Web/Web4_RPCClient_2.go
+++ Go_Web/Web4_RPCClient_2.go
@@ -15,24 +15,31 @@ type Quotient struct {
 	Quo, Rem int
 }
 
+func checkError(err error) {
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Fatal error %s", err.Error())
+		os.Exit(1)
+	}
+}
+
 func main() {
 	if len(os.Args) != 2 {
 		fmt.Fprintf(os.Stderr, "Usage: %s ip:port", os.Args[0])
 	}
 	addrArg := os.Args[1]
-	conn, err := rpc.DialHTTP("tcp", addrArg)
-	if err != nil {
-		log.Fatal("dialing", err)
-	}
+	conn, err := rpc.Dial("tcp", addrArg)
+	checkError(err)
+	// Synchronous call
 	args := Args{17, 8}
 	var reply int
-	err = conn.Call("Arith.Multiply", &args, &reply)
+	err = conn.Call("Arith.Multiply", args, &reply)
 	if err != nil {
 		log.Fatal("arith error:", err)
 	}
 	fmt.Printf("Arith: %d*%d=%d\n", args.A, args.B, reply)
+
 	var quot Quotient
-	err = conn.Call("Arith.Divide", &args, &quot)
+	err = conn.Call("Arith.Divide", args, &quot)
 	if err != nil {
 		log.Fatal("arith error:", err)
 	}
